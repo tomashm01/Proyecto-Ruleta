bc53a7672b7a272bab181ebcb3dabc2d3f5ad4fa
Tomás Hidalgo <tomashidalgomartin@gmail.com>
2021-02-16 01:52:26 +0100

    VOlvemos al principio

--- src/project/Main.java
+++ src/project/Main.java
@@ -3,7 +3,7 @@ package project;
 import java.util.Scanner;
 public class Main{
   public static void main(String[] args) {
-    
+    System.out.println("HOla");
   }
   static void menu() {
     System.out.println(ConsoleColors.BLUE_BOLD+"                --PRINCIPAL MENU-- ");
