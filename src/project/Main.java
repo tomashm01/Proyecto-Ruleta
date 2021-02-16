b4368bf15649b35b8e79e29e09b569b13bcad111
Tomás Hidalgo <tomashidalgomartin@gmail.com>
2021-02-16 17:05:11 +0100

    Added: Menu cambios

--- src/project/Main.java
+++ src/project/Main.java
@@ -2,24 +2,41 @@ package project;
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
-		mainMenu();
+	  int n;
+	  do {
+	    n=mainMenu();
+	    switch(n){
+	      case 1:
+	        System.out.println("Ruleta");
+	        break;
+	      case 2:
+	        System.out.println("Persona");
+	        break;
+	      case 3:
+	        System.out.println("Adios");
+	        break;
+	      default:
+	        System.out.println("Error en la entrada");
+	        break;
+	    }
+	  }while(n!=3);
 	}
 
-	static void mainMenu() {
+	static int mainMenu() {
 		Menu main = new Menu("--MAIN MENU--", "Play roulette", "Person menu");
-		main.manage();
+		return main.manage();
 	}
-	static void rouletteMenu() {
+	static int rouletteMenu() {
 	  Menu roulette=new Menu("--ROULETTE MENU--","Color bet","Even or odd bet","Higher or lower bet");
-	  roulette.manage();
+	  return roulette.manage();
 	}
-	static void personMenu() {
+	static int personMenu() {
 	  Menu person=new Menu("--PERSON MENU--","Add player","Erase player","Print all the players","Change options of the person");
-	  person.manage();
+	  return person.manage();
 	}
-	static void changePersonMenu() {
+	static int changePersonMenu() {
 	  Menu change=new Menu("--CHANGE PERSON MENU--","Change dni","Change name","Change surnames","Change age","Change location");
-	  change.manage();
+	  return change.manage();
 	}
 	
 	
