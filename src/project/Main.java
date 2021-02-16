2939c650b54433622757e94ff36370697ec1648b
Tomás Hidalgo <tomashidalgomartin@gmail.com>
2021-02-16 16:46:02 +0100

    Added: Objetos menus creados

--- src/project/Main.java
+++ src/project/Main.java
@@ -2,43 +2,24 @@ package project;
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
-		menu();
+		mainMenu();
 	}
 
-	static void menu() {
-		Menu mainMenu = new Menu("--MAIN MENU--", "Play roulette", "Person menu");
-		mainMenu.manage();
+	static void mainMenu() {
+		Menu main = new Menu("--MAIN MENU--", "Play roulette", "Person menu");
+		main.manage();
 	}
-
-	static void menuChanges() {
-		System.out.println(ConsoleColors.BLUE_BOLD + "                --PERSON CHANGE MENU-- ");
-		System.out.println(ConsoleColors.RESET);
-		System.out.println(ConsoleColors.YELLOW + "1.Change name");
-		System.out.println(ConsoleColors.RESET);
-		System.out.println(ConsoleColors.YELLOW + "2.Change surnames");
-		System.out.println(ConsoleColors.RESET);
-		System.out.println(ConsoleColors.YELLOW + "3.Change age");
-		System.out.println(ConsoleColors.RESET);
-		System.out.println(ConsoleColors.YELLOW + "4.Change location");
-		System.out.println(ConsoleColors.RESET);
-		System.out.println(ConsoleColors.YELLOW + "5.Change Dni");
-		System.out.println(ConsoleColors.RESET);
-		System.out.println(ConsoleColors.RED + "6.Go back");
-		System.out.println(ConsoleColors.RESET);
+	static void rouletteMenu() {
+	  Menu roulette=new Menu("--ROULETTE MENU--","Color bet","Even or odd bet","Higher or lower bet");
+	  roulette.manage();
 	}
-
-	static void menuPersona() {
-		System.out.println(ConsoleColors.BLUE_BOLD + "                --PERSON MENU-- ");
-		System.out.println(ConsoleColors.RESET);
-		System.out.println(ConsoleColors.WHITE + "1.Add Player");
-		System.out.println(ConsoleColors.RESET);
-		System.out.println(ConsoleColors.WHITE + "2.Erase player");
-		System.out.println(ConsoleColors.RESET);
-		System.out.println(ConsoleColors.WHITE + "3.Print all the players");
-		System.out.println(ConsoleColors.RESET);
-		System.out.println(ConsoleColors.WHITE + "4.Change options of the person");
-		System.out.println(ConsoleColors.RESET);
-		System.out.println(ConsoleColors.RED + "5.Go back");
-		System.out.println(ConsoleColors.RESET);
+	static void personMenu() {
+	  Menu person=new Menu("--PERSON MENU--","Add player","Erase player","Print all the players","Change options of the person");
+	  person.manage();
 	}
+	static void changePersonMenu() {
+	  Menu change=new Menu("--CHANGE PERSON MENU--","Change dni","Change name","Change surnames","Change age","Change location")
+	}
+	
+	
 }
